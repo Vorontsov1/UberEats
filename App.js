44f02e22830675a16e63451365a212504cc1ce44
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2023-03-08 21:29:11 +0200

    added_images

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* Resraurant item */}
+      <View style={styles.restaurantContainer}>
+        <Image
+          source={{
+            uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/uber-eats/restaurant1.jpeg",
+          }}
+          style={styles.image}
+        />
+        <Text style={styles.title}>El Cabo Coffe Bar Tres De Mayo</Text>
+        <Text style={styles.subtitle}>$1.99 15-30 minutes</Text>
+      </View>
+      <View style={styles.restaurantContainer}>
+        <Image
+          source={{
+            uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/uber-eats/restaurant1.jpeg",
+          }}
+          style={styles.image}
+        />
+        <Text style={styles.title}>El Cabo Coffe Bar Tres De Mayo</Text>
+        <Text style={styles.subtitle}>$1.99 15-30 minutes</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +33,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'yellow',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#81BECE",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+  },
+  restaurantContainer: {
+    width: "100%",
+    marginVertical: 10,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 5 / 3,
+    marginBottom: 5,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: 500, 
+    marginVertical: 5,
+  },
+  subtitle: {
+    color: 'grey', 
   },
 });
