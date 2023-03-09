2363daf031ec4e6a8ee655a9bd507b9e21298878
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2023-03-09 12:56:11 +0200

    added_images

--- src/components/DishListItem/index.js
+++ src/components/DishListItem/index.js
@@ -3,31 +3,43 @@ import { View, Text, StyleSheet, Image } from "react-native";
 const DishListItem = ({ dish }) => {
   return (
     <View style={styles.container}>
-      <Text style={styles.name}>{dish.name}</Text>
-      <Text style={styles.description}>{dish.description}</Text>
-      <Text style={styles.price}>$ {dish.price}</Text>
+      <View style={{ flex: 1 }}>
+        <Text style={styles.name}>{dish.name}</Text>
+        <Text style={styles.description} numberOfLines={2}>
+          {dish.description}
+        </Text>
+        <Text style={styles.price}>$ {dish.price}</Text>
+      </View>
+      {dish.image && (
+        <Image source={{ uri: dish.image }} style={styles.image} />
+      )}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-        borderBottomColor: 'lighgrey',
-        borderBottomWidth: 1,
-        marginHorizontal: 23,
-        paddingVertical: 13,
+    borderBottomColor: "lighgrey",
+    borderBottomWidth: 1,
+    marginHorizontal: 23,
+    paddingVertical: 13,
+    flexDirection: "row",
   },
-    name: {
-      letterSpacing: 0.5,
+  name: {
+    letterSpacing: 0.5,
     fontWeight: "600",
     fontSize: 16,
   },
   description: {
-      color: "grey",
-      marginVertical: 10,
+    color: "grey",
+    marginVertical: 10,
   },
   price: {
     fontSize: 16,
+  },
+  image: {
+    height: 80,
+    aspectRatio: 1,
   },
 });
 
