1e833db31a914a97834f18886889dde5a414d404
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2023-03-10 18:14:56 +0200

    added_orderListItems

--- src/components/OrderListItem/index.js
+++ src/components/OrderListItem/index.js
@@ -1,9 +1,19 @@
-import { Text, View } from 'react-native'
+import { Text, View, Image } from 'react-native'
 
-const OrderListItem = () => {
+const OrderListItem = ({order}) => {
     return (
-      <View>
-        <Text>OrderListItem</Text>
+      <View style={{ flexDirection: "row", margin: 15, alignItems: 'center', }}>
+        <Image
+          source={{ uri: order.Restaurant.image }}
+          style={{ width: 75, height: 75, marginRight: 10 }}
+        />
+        <View>
+          <Text style={{ fontWeight: "600", fontSize: 16 }}>
+            {order.Restaurant.name}
+          </Text>
+          <Text style={{marginVertical: 5,}}>3 items &#8226; $38.45</Text>
+          <Text>2 days ago &#8226; {order.status}</Text>
+        </View>
       </View>
     );
 }
