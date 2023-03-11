91bb2e33e0bcd50a016fd4ae1a48842e665b55ce
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2023-03-11 21:17:36 +0200

    added_tabBar

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,10 +1,15 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import HomeScreen from "../screens/HomeScreen";
 import RestaurantDetailsPage from "../screens/RestaurantDetails";
 import DishDetailsScreen from "../screens/DishDetailsScreen";
 import Basket from "../screens/Basket";
 import OrderScreen from "../screens/OrdersScreen";
 import OrderDetails from "../screens/OrderDetails";
+import { Ionicons } from '@expo/vector-icons';
+import { Fontisto } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
+import { StyleSheet } from 'react-native';
 
 const Stack = createNativeStackNavigator();
 
@@ -22,4 +27,50 @@ const RootNavigator = () => {
     );
 };
 
-export default RootNavigator;
+const Tab = createMaterialBottomTabNavigator();
+
+const HomeTabs = () => {
+  return (
+    <Tab.Navigator
+      barStyle={styles.tabBarStyle}
+      // activeColor="#FDB10B"
+      // inactiveColor="#FD292F"
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="md-home" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Order"
+        component={OrderScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Fontisto name="shopping-basket" size={24} color="black" />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={OrderScreen}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <AntDesign name="profile" size={24} color="black" />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+const styles = StyleSheet.create({
+  tabBarStyle: {
+    backgroundColor: "#A0C4DC", // replace with any color of your choice
+  },
+});
+
+export default HomeTabs;
